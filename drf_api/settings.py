2561a513d103d854f1e6d7eb28f1f9323016ab5e
AnneKammenhed <anne.juriado@gmail.com>
2023-02-20 09:26:36 +0000

    hide secret key

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -29,7 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p*z7iac+=&=jp*ty@u@=+gi8$rzhtn#%tjh7btfe%qn&@(_-cg'
+SECRET_KEY = {
+    'SECRET_KEY': os.environ.get('SECRET_KEY')
+}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
