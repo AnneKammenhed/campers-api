633633143cc6a6ed56eafecf215d6e367e586aef
AnneKammenhed <anne.juriado@gmail.com>
2023-02-20 11:32:35 +0000

    add detail view and http404 response if profile non existent

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,7 +4,9 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-
+    """
+    Lists all the fields of the camper profiles.
+    """
     class Meta:
         model = Profile
         fields = '__all__'
