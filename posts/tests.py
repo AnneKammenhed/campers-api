b3d26035c794618c8c6d64324b36e580b4f0c0d8
AnneKammenhed <anne.juriado@gmail.com>
2023-02-21 11:38:26 +0000

    add test to check that all posts are listed

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+# Create user anne and test to list all the posts
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='anne', password='cat')
+
+    def test_list_posts(self):
+        anne = User.objects.get(username='anne')
+        Post.objects.create(owner=anne, camping_title='campervan')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
